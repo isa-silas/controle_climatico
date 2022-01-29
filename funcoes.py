f58b2ec99005f828e7424418d1589f1d6023252a
Isabella Silas <isabella.silasiss@gmail.com>
2022-01-29 11:04:23 -0300

    Agora ha uma variavel que guarda qual ventilador esta sendo alterado

--- funcoes.py
+++ funcoes.py
@@ -1,17 +1,29 @@
-# from main import painel_controle as painel
-# from main import timer as controle
-
-
 class Painel:
+    """
+    [Resumo]
+
+            Essa classe serve para conectar o painel de controle com as telas de 
+            configuracao de cada ventilador
+            
+    [Obs] 
+            self.qual_vent = guarda qual ventilador esta sendo configurado
+            
+            self.janela_controle = tela do timer para user comum e de status dos 
+            ventiladores para adm   
+    """
     def __init__(self,painel,controle):
+        self.qual_vent = 0
         self.janela_controle = controle
-        self.vent_escritorio = painel.btn_v1
-        self.vent_oficina = painel.btn_v2
-        self.vent_escritorio.clicked.connect(self.abrir_controle)
-        self.vent_oficina.clicked.connect(self.abrir_controle)
+        
+        self.vent_1 = painel.btn_v1
+        self.vent_2 = painel.btn_v2
+        
+        self.vent_1.clicked.connect(lambda: self.abrir_controle(1))
+        self.vent_2.clicked.connect(lambda: self.abrir_controle(2))
         
 
-    def abrir_controle(self):
+    def abrir_controle(self, ventilador: int):
         self.janela_controle.show()
+        self.qual_vent = ventilador
 
 
--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import sys
 from funcoes import Painel
 
 
-app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
+app = QtWidgets.QApplication(sys.argv) 
+
 login = uic.loadUi('tela_login.ui') # Load the .ui file
 painel_controle = uic.loadUi('tela_painel_controle.ui')
 painel_controle_adm = uic.loadUi('tela_painel_controle_adm.ui')
 timer = uic.loadUi('tela_liga_timer.ui')
-painel_controle.show() # Show the GUI
-painel = Painel(painel_controle,timer)
+
+# Teste painel controle
+# painel_controle.show() 
+# painel = Painel(painel_controle,timer)
+
 app.exec_() # Start the application
 
 
