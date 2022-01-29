c39d3a0bbd5df6ed57b712a30151cde68ff50ad6
Isabella Silas <isabella.silasiss@gmail.com>
2022-01-29 09:34:21 -0300

    Alteracao do nome do arquivo main

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import sys
 
 app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
 login = uic.loadUi('tela_login.ui') # Load the .ui file
-painel_controle = uic.loadUi('tela_login.ui')
+painel_controle = uic.loadUi('tela_painel_controle.ui')
+painel_controle_adm = uic.loadUi('tela_painel_control_adm.ui')
+timer = uic.loadUi('tela_liga_timer.ui')
 login.show() # Show the GUI
 app.exec_() # Start the application
 
